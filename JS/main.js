5d81e3ee3d8c9b00096cbe6f7b97d9cbc8320386
RickeyRM <97_roman_97@mail.ru>
2021-06-25 15:15:11 +0300

    map

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,4 @@
 ymaps.ready(init);
-let storage = localStorage;
 
 function init (){
     let map = new ymaps.Map('map', {
@@ -11,40 +10,68 @@ function init (){
     map.events.add('click', function (e) {
         if (!map.balloon.isOpen()) {
             var coords = e.get('coords');
-            map.balloon.open(coords, {
-                contentBody: `<div class="balloon">
+            let placemark = new ymaps.Placemark(coords, {
+                balloonContent: `<div class="balloon">
                                     <h1 class="title">Отзыв:</h1>
+                                    ${renderReviwes()}
                                     <input class="input input__name" type="text" placeholder="Укажите ваше имя">
                                     <input class="input input__place" type="text" placeholder="Укажите место">
                                     <textarea class="textarea textarea__reviews" placeholder ="Оставить отзыв"></textarea>
                                     <button class="button">Добавить</button>
                                 </div>`,
-                contentFooter:'<sup>Щелкните еще раз для выхода</sup>'
-                
             });
+            map.geoObjects.add(placemark);
+            addReviews(coords);
         } else {
             map.balloon.close();
         }
-        document.addEventListener('click', (evt) => {
-
-            const name = document.querySelector('.input__name');
-            const place = document.querySelector('.input__place');
-            const reviews = document.querySelector('.textarea__reviews');
-
-            if (evt.target.classList.contains('button')) {
-
-                let saveReviews = [];
-                let storage = localStorage;
-                storage.data = JSON.stringify({
-                    name: name.value,
-                    place: place.value,
-                    reviews: reviews.value
-                })
-                saveReviews.push(JSON.parse(storage.data));
-                console.log(saveReviews)
-            }
-          })
-    });   
+        
+        
+    });  
+    
 };
 
+let saveReviews = [];
+let storage = localStorage;
 
+
+        
+function addReviews (key){
+    document.addEventListener('click', (evt) => {
+
+        const name = document.querySelector('.input__name');
+        const place = document.querySelector('.input__place');
+        const reviews = document.querySelector('.textarea__reviews');
+    
+        if (evt.target.classList.contains('button')) {
+            // if(storage.data){
+            //     saveReviews.push(JSON.parse(storage.data));
+                
+            // }
+            saveReviews.push({
+                name: name.value,
+                place: place.value,
+                reviews: reviews.value
+            })
+            storage[key] = JSON.stringify(saveReviews)
+
+            name.value = '';
+            place.value = '';
+            reviews.value = '';
+    
+            
+        }
+    
+    })
+}
+ 
+const renderReviwes = () =>{
+    if(storage.data){
+        let returnReviews = JSON.parse(storage.data);
+        console.log(returnReviews)
+
+        return `<div>${returnReviews[0].name}</div>
+                <div>${returnReviews[0].place}</div>
+                <div>${returnReviews[0].reviews}</div>`
+    }
+}
